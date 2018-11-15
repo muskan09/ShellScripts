5b7d625acb62ca0e7d07347f44a8226197b8dd4d
muskan09 <muskankalsi9@gmail.com>
2018-11-15 21:01:20 +0530

    shell script

---
+++ ext1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter number : "
+read n
+sd=0
+sum=0
+while [ $n -gt 0 ]
+do
+sd=$(( $n % 10 )) 
+n=$(( $n / 10 ))  
+sum=$(( $sum + $sd ))
+done
+echo "Sum of all digit is $sum"
